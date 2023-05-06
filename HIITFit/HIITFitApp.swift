27882e74b6c36e9212cdf2d9fec1c50987e229ef
Eduardo Rodriguez Gonzalez <soporte@decodeapp.net>
2023-05-06 21:14:53 +0200

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Eduardo Rodriguez on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
